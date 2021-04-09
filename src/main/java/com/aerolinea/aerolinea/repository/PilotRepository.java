31ae4190aee051876cd3b316f4b91bc18152e0ad
Jokasta Carolina <jokasta_carolina@hotmail.com>
2021-04-08 22:20:18 -0300

    elimine variable sin usar

--- src/main/java/com/aerolinea/aerolinea/repository/PilotRepository.java
+++ src/main/java/com/aerolinea/aerolinea/repository/PilotRepository.java
@@ -4,5 +4,5 @@ import com.aerolinea.aerolinea.model.Pilot;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PilotRepository extends CrudRepository<Pilot,Integer> {
-int edad =1;}
+}
 
